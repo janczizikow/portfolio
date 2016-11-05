b7b2490f862271b207ef2fe65ce8726312dea562
Jan Czizikow <Johnny@Johnnys-MacBook-Pro.local>
2016-11-06 03:50:04 +0800

    SASS + general structure

---
+++ config.rb
@@ -0,0 +1,13 @@
+require "susy"
+
+project_type = :stand_alone
+http_path = "/"
+sass_dir = "css"
+css_dir = "css"
+images_dir ="img"
+fonts_dir ="fonts"
+javscripts_dir ="js"
+line_cimments = false
+preferred_syntax = :scss
+output_style = :expanded
+relative_assets = true
