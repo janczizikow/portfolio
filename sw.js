a3c877d35d4aff05815516acf6b5b2e036507153
Jan Czizikow <Johnny@Johnnys-MacBook-Pro-2.local>
2017-05-30 00:04:50 +0800

    Google Optimise - bottleneck for the meaningful paint
    Reduced the Google Optimize setTimeout to 0.5s

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'v05';
+var CACHE_NAME = 'v06';
 var urlsToCache = [
 	'/',
 	'/assets/css/main.css',
@@ -20,7 +20,7 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('activate',function(event) {
-	var cacheWhitelist = ['v05'];
+	var cacheWhitelist = ['v06'];
 
 	event.waitUntil(
 		caches.keys().then(function(cacheNames) {
