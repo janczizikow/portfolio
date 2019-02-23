f6f2e093387024ddcd5fe37d94f14a47f5fb22f3
Jan Czizikow <jan@gohidoc.com>
2019-02-23 20:14:44 +0100

    Fix codepen footer link & open footer links in new tab

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,13 +8,25 @@ import FooterLink from './FooterLink';
 const Footer = () => (
   <Box is="footer" py={58} bg="bgAlt" color="white">
     <Flex alignItems="center" justifyContent="center">
-      <FooterLink href="https://github.com/janczizikow/">
+      <FooterLink
+        href="https://github.com/janczizikow/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <FiGithub size={24} />
       </FooterLink>
-      <FooterLink href="/about/">
+      <FooterLink
+        href="https://codepen.io/hollow3d/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <FiCodepen size={24} />
       </FooterLink>
-      <FooterLink href="https://www.linkedin.com/in/janczizikow/">
+      <FooterLink
+        href="https://www.linkedin.com/in/janczizikow/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <FiLinkedin size={24} />
       </FooterLink>
     </Flex>
