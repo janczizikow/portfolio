c416dd2bd38e60771f873f93b0bbb572c9dbe993
Jan Czizikow <jan.gmail@czizikow.com>
2019-03-02 15:18:53 +0100

    Fix: Flex defalut props

--- src/components/UI/Flex.js
+++ src/components/UI/Flex.js
@@ -1,5 +1,6 @@
 import styled from '@emotion/styled';
 import {
+  display,
   flexWrap,
   flexDirection,
   alignItems,
@@ -8,6 +9,7 @@ import {
 import Box from './Box';
 
 const Flex = styled(Box)`
+  ${display}
   ${flexWrap}
   ${flexDirection}
   ${alignItems}
@@ -19,6 +21,9 @@ Flex.propTypes = {
   ...flexDirection.propTypes,
   ...alignItems.propTypes,
   ...justifyContent.propTypes,
+};
+Flex.defaultProps = {
+  display: 'flex',
 };
 Flex.displayName = 'Flex';
 
