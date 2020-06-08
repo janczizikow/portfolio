6718ba142ff6c6cf7edd5bfe5b734db31c1d39c2
Jan Czizikow <jan.czizikow@gmail.com>
2020-06-08 22:52:43 +0200

    seeds

--- server/config/functions/bootstrap.js
+++ server/config/functions/bootstrap.js
@@ -1,4 +1,3 @@
-'use strict';
 require('dotenv').config();
 /**
  * An asynchronous bootstrap function that runs before
@@ -9,5 +8,88 @@ require('dotenv').config();
  *
  * See more details here: https://strapi.io/documentation/v3.x/concepts/configurations.html#bootstrap
  */
+const fs = require('fs');
+const { projects } = require('../../seed/seed');
 
-module.exports = () => {};
+const findPublicRole = async () => {
+  const result = await strapi
+    .query('role', 'users-permissions')
+    .findOne({ type: 'public' });
+  return result;
+};
+
+const setDefaultPermissions = async () => {
+  const role = await findPublicRole();
+  const permissions = await strapi
+    .query('permission', 'users-permissions')
+    .find({ type: 'application', role: role.id });
+  await Promise.all(
+    permissions.map(p =>
+      strapi
+        .query('permission', 'users-permissions')
+        .update({ id: p.id }, { enabled: true })
+    )
+  );
+};
+
+const isFirstRun = async () => {
+  const pluginStore = strapi.store({
+    environment: strapi.config.environment,
+    type: 'type',
+    name: 'setup',
+  });
+  const initHasRun = await pluginStore.get({ key: 'initHasRun' });
+  await pluginStore.set({ key: 'initHasRun', value: true });
+  return !initHasRun;
+};
+
+const getFilesizeInBytes = filepath => {
+  const stats = fs.statSync(filepath);
+  const fileSizeInBytes = stats.size;
+  return fileSizeInBytes;
+};
+
+const getFilePath = fileName => {
+  return `./seed/images/${fileName}`;
+};
+
+const createSeedData = async () => {
+  const projectsPromises = projects.map(project => {
+    const {
+      thumbnail: { imageFileName, mimeType },
+      photos,
+      ...rest
+    } = project;
+    const thumbnailFilepath = getFilePath(imageFileName);
+    const thumbnailImage = {
+      path: getFilePath(imageFileName),
+      name: imageFileName,
+      size: getFilesizeInBytes(thumbnailFilepath),
+      type: mimeType,
+    };
+    const files = {
+      thumbnail: thumbnailImage,
+      photos: photos.map(photo => ({
+        path: getFilePath(photo.imageFileName),
+        name: photo.imageFileName,
+        size: getFilesizeInBytes(getFilePath(photo.imageFileName)),
+        type: photo.mimeType,
+      })),
+    };
+    return strapi.services.project.create(
+      {
+        ...rest,
+      },
+      { files }
+    );
+  });
+  await Promise.all(projectsPromises);
+};
+
+module.exports = async () => {
+  const shouldSetDefaultPermissions = await isFirstRun();
+  if (shouldSetDefaultPermissions) {
+    await setDefaultPermissions();
+    await createSeedData();
+  }
+};
---
+++ server/seed/seed.js
@@ -0,0 +1,172 @@
+module.exports = {
+  projects: [
+    {
+      name: 'Minimal Portfolio',
+      slug: 'minimal-portfolio',
+      description:
+        'Web development of simple, responsive and minimalistic portfolio website, which could be used instead of ordinary pdf file. Developed using bootstrap framework components (grid, navigation).',
+      date: '2016-10-26',
+      links: [
+        {
+          url: 'http://www.marinesanzvico.com/?ref=www.janczizikow.com',
+          text: 'View Site',
+        },
+      ],
+      thumbnail: {
+        mimeType: 'image/jpeg',
+        imageFileName: 'minimal-portfolio.jpg',
+      },
+      photos: [
+        {
+          mimeType: 'image/jpeg',
+          imageFileName: 'minimal-portfolio-1.jpg',
+        },
+        {
+          mimeType: 'image/jpeg',
+          imageFileName: 'minimal-portfolio-2.jpg',
+        },
+        {
+          mimeType: 'image/jpeg',
+          imageFileName: 'minimal-portfolio-3.jpg',
+        },
+      ],
+    },
+    {
+      name: 'The clinic',
+      slug: 'the-clinic',
+      description:
+        'The clinic is a premier high-end physical therapy, sports medicine, and wellness center, based in Shanghai. The objective of the project was to design and develop a responsive newsletter template, that would match the high-end standards of the clinic and remain coherent with its visual i dentity, while being entirely editable in Mailchimp interface.',
+      date: '2016-11-21',
+      links: null,
+      thumbnail: {
+        mimeType: 'image/jpeg',
+        imageFileName: 'the-clinic.jpg',
+      },
+      photos: [
+        {
+          mimeType: 'image/jpeg',
+          imageFileName: 'the-clinic-1.jpg',
+        },
+        {
+          mimeType: 'image/jpeg',
+          imageFileName: 'the-clinic-2.jpg',
+        },
+        {
+          mimeType: 'image/jpeg',
+          imageFileName: 'the-clinic-3.jpg',
+        },
+      ],
+    },
+    {
+      name: 'Blanccstate',
+      date: '2018-01-14',
+      slug: 'blanccstate',
+      description:
+        'Web design and development of a landing page for an artist. The EP wasn’t released yet, but the website is up and running. Thanks to modern tech (GatsbyJS & React) the browsing experience is lightning fast! Check it out:',
+      links: [
+        {
+          text: 'View on Github',
+          url: 'https://github.com/janczizikow/blancc-state',
+        },
+        {
+          text: 'View Site',
+          url: 'https://blancc-state.netlify.com/',
+        },
+      ],
+      thumbnail: {
+        mimeType: 'image/jpeg',
+        imageFileName: 'blanccstate.jpg',
+      },
+      photos: [
+        {
+          mimeType: 'image/jpeg',
+          imageFileName: 'blanccstate-1.jpg',
+        },
+        {
+          mimeType: 'image/jpeg',
+          imageFileName: 'blanccstate-2.jpg',
+        },
+        {
+          mimeType: 'image/jpeg',
+          imageFileName: 'blanccstate-3.jpg',
+        },
+        {
+          mimeType: 'image/jpeg',
+          imageFileName: 'blanccstate-4.jpg',
+        },
+      ],
+    },
+    {
+      name: 'Jekyll Sleek',
+      date: '2018-02-18',
+      slug: 'jekyll-sleek',
+      description:
+        'Design and development of a gem-based jekyll theme focused on speed performance and minimalistic style. Heavily inspired by well, my own website.',
+      links: [
+        {
+          text: 'View on Github',
+          url: 'https://github.com/janczizikow/sleek',
+        },
+        {
+          text: 'View Demo Site',
+          url: 'https://janczizikow.github.io/sleek/',
+        },
+      ],
+      thumbnail: {
+        mimeType: 'image/jpeg',
+        imageFileName: 'jekyll-sleek.jpg',
+      },
+      photos: [
+        {
+          mimeType: 'image/jpeg',
+          imageFileName: 'jekyll-sleek-1.jpg',
+        },
+        {
+          mimeType: 'image/jpeg',
+          imageFileName: 'jekyll-sleek-2.jpg',
+        },
+      ],
+    },
+    {
+      name: 'Skatespots',
+      date: '2018-08-31',
+      slug: 'skatespots',
+      description:
+        // eslint-disable-next-line quotes
+        "As a final project for my Ruby on Rails coding bootcamp I've built a shameless Airbnb copy. There's just a tiny twist - the platform focuses on skatespots rather then flats.",
+      links: [
+        {
+          text: 'View Site',
+          url:
+            'https://rails-skatespots.herokuapp.com/?ref=www.janczizikow.com',
+        },
+      ],
+      thumbnail: {
+        mimeType: 'image/jpeg',
+        imageFileName: 'skatespots.jpg',
+      },
+      photos: [
+        {
+          mimeType: 'image/jpeg',
+          imageFileName: 'skatespots-1.jpg',
+        },
+        {
+          mimeType: 'image/jpeg',
+          imageFileName: 'skatespots-2.jpg',
+        },
+        {
+          mimeType: 'image/jpeg',
+          imageFileName: 'skatespots-3.jpg',
+        },
+        {
+          mimeType: 'image/jpeg',
+          imageFileName: 'skatespots-4.jpg',
+        },
+        {
+          mimeType: 'image/jpeg',
+          imageFileName: 'skatespots-5.jpg',
+        },
+      ],
+    },
+  ],
+};
